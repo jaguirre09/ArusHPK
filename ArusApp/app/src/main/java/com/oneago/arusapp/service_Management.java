f733abd61a7c687957adc39f1355abc365f9a7a2
juan aguirre <jaguirre@oneago.com>
2020-05-13 16:08:35 -0500

    finished manage your services

---
+++ ArusApp/app/src/main/java/com/oneago/arusapp/service_Management.java
@@ -0,0 +1,14 @@
+package com.oneago.arusapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class service_Management extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service__management);
+    }
+}
