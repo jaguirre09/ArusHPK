e39ddd70d32ad3b419058ea6cbe9e96bbde1b296
juan aguirre <jaguirre@oneago.com>
2020-05-14 14:10:19 -0500

    update job

--- ArusApp/app/src/main/java/com/oneago/arusapp/service_Management.java
+++ ArusApp/app/src/main/java/com/oneago/arusapp/service_Management.java
@@ -2,7 +2,9 @@ package com.oneago.arusapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class service_Management extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class service_Management extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service__management);
+    }
+    public void main_exit(View v) {
+
+        Intent main_exit = new Intent(this, MainActivity.class);
+        startActivity(main_exit);
+
     }
 }
