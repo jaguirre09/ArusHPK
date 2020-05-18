7c8f947da66a269cd13bd08827888486347c8e5f
jrueda@oneago.com <juan78030>
2020-05-18 11:47:50 -0500

    AddSurvey.php INSERT INTO ok

---
+++ ArusAdminPortal/api/classes/AddSurvey.php
@@ -0,0 +1,27 @@
+<?php
+
+
+class AddSurvey
+{
+    public function __construct(string $json)
+    {
+        $con = null;
+        include_once "connection.php"; // include $con
+        $query = "INSERT INTO SURVEYS (QUESTIONS) VALUES (?);";
+        $prepare = mysqli_stmt_init($con);
+        if (mysqli_stmt_prepare($prepare, $query)) {
+            mysqli_stmt_bind_param($prepare, "s", $json);
+            mysqli_stmt_execute($prepare);
+            if (!empty(mysqli_stmt_error($prepare))) {
+                $message = mysqli_stmt_error($prepare);
+                mysqli_stmt_close($prepare);
+                return $message;
+            }
+            mysqli_stmt_close($prepare);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
